9945a232ade4bffe9fb9edf56596b4609b40d155
Костенков Даниил Дмитриевич <shlyapp@star.localdomain>
2023-11-01 10:48:28 +0400

    Example of use

--- src/main.py
+++ src/main.py
@@ -1,9 +1,15 @@
-from parser import get_opinions
+from parser import get_product_id, get_opinions
 
 
 def main():
-    get_opinions()
+    url ="https://www.dns-shop.ru/product/bf41006e0b2fed20/videokarta-palit-geforce-rtx-4060-dual-ne64060019p1-1070d/"
+    id = get_product_id(url)
 
+    if id == -1:
+        print("Not find id")
+
+    opinions = get_opinions(id)
+    print(opinions[0].plus)
 
 if __name__ == "__main__":
     main()
