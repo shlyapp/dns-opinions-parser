e9381ad4f13f47d837103cc09adbba00b56c9a25
Костенков Даниил Дмитриевич <78727203+shlyapp@users.noreply.github.com>
2023-11-02 01:25:55 +0400

    Async parser

--- src/parser.py
+++ src/parser.py
@@ -3,10 +3,13 @@ import requests
 import math
 from marshmallow_dataclass import class_schema
 from bs4 import BeautifulSoup
-import re
 from seleniumbase import Driver
-from urllib.parser import quote
+from urllib.request import quote
 from typing import List
+import aiohttp
+import asyncio
+
+from asyncio_throttle import Throttler
 
 from schemas import Opinion
 from config import get_data_json, headers
@@ -26,19 +29,19 @@ def get_id_products(search: str) -> List[int]:
     pages = int(pages[-1]['data-page-number'])
     product_id = []
 
-    for page in range(1, pages):
+    for page in range(0, pages + 1):
         url = "https://www.dns-shop.ru/search/?q={0}&p={1}".format(quote(search), quote(str(page)))
         soup = BeautifulSoup(response, "html.parser")
         products = soup.find_all("div", {"data-id": "product",
                                         "class": "catalog-product ui-button-widget"})
-
+        
         for product in products:
             product_id.append(product['data-product'])
 
     return product_id
 
 
-def get_opinions(id: int) -> List[Opinion]:
+def get_response_count(id: str) -> int:
     session = requests.Session()
     response = session.post(
             url='https://www.dns-shop.ru/opinion/opinions/get/',
@@ -48,25 +51,73 @@ def get_opinions(id: int) -> List[Opinion]:
     data = response.json()
 
     if response.status_code != 200 or data["result"] == False:
-        return []
+        return 0
 
     total_opinions = data.get("data").get("totalCount")
     response_count = math.ceil(total_opinions / 10)
-    opinions = []
+
+    return response_count
+
+
+def get_opinions(id: str):
+    response_count = get_response_count(id)
+    session = requests.Session()
+    opinions = [] 
 
     for i in range(response_count):
-        offset = i * 10
-        response = session.post(
-            url='https://www.dns-shop.ru/opinion/opinions/get/',
-            data=get_data_json(object_id=id,
-                               offset=offset),
-            headers=headers).json()
+        response = session.post(url="https://www.dns-shop.ru/opinion/opinions/get/",
+                data=get_data_json(object_id=id,
+                                   offset=i*10),
+                headers=headers).json()
 
+        if response["result"] == False:
+            return
+    
         opinions_data = response.get("data").get("opinions")
-
+            
+        if opinions_data is None:
+            return
+           
         for opinion_data in opinions_data:
             schema = class_schema(Opinion)()
             opinion = schema.load(opinion_data)
             opinions.append(opinion)
 
     return opinions
+
+
+async def get_opinions_async(session, id: str, offset: int, throrrler):
+    opinions = []
+    async with throrrler:
+        async with session.post(url="https://www.dns-shop.ru/opinion/opinions/get/",
+                                data=get_data_json(object_id=id,
+                                offset=offset),
+                                headers=headers) as response:
+            response = await response.json()
+
+            if response["result"] == False:
+                return
+    
+            opinions_data = response.get("data").get("opinions")
+            
+            if opinions_data is None:
+                return
+           
+            for opinion_data in opinions_data:
+                schema = class_schema(Opinion)()
+                opinion = schema.load(opinion_data)
+                opinions.append(opinion)
+
+    return opinions
+       
+
+async def gather_data(id: str):
+    response_count = get_response_count(id)
+    tasks = []
+    throttler = Throttler(rate_limit=22, period=1)
+    async with aiohttp.ClientSession() as session:
+        for i in range(response_count):
+            task = asyncio.create_task(get_opinions_async(session, id, i * 10, throttler))
+            tasks.append(task)
+        
+        await asyncio.gather(*tasks)
